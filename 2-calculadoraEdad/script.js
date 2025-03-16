7989dc0470d87c045c6b29caa7e1debb6a783f72
ikabeee <carlglz30@gmail.com>
2025-03-16 16:03:12 -0500

    Mejoré la logica del calculo de horas

--- 2-calculadoraEdad/script.js
+++ 2-calculadoraEdad/script.js
@@ -14,33 +14,32 @@ var fechaActual = {
     hora: new Date().getHours()
 }
 
-const fechaNacimiento =()=>{
+const fechaNacimiento = () => {
     const dia = parseInt(prompt("Ingresa tu dia de nacimiento (Número del día)"));
     const mes = parseInt(prompt("Ingresa tu mes de nacimiento (Número del mes)"));
     const anio = parseInt(prompt("Ingresa tu año de nacimiento (Número del año)"));
     const hora = parseInt(prompt("Ingresa tu hora de nacimiento (0-23)"));
-    const fechaUsuario = {mes: mes, hora: hora, dia: dia, anio: anio};
+    const fechaUsuario = { mes: mes, hora: hora, dia: dia, anio: anio };
     return fechaUsuario;
 }
 console.log(fechaActual)
-const calcularEdad =()=>{
-   const {mes, hora, dia, anio} = fechaNacimiento();
+const calcularEdad = () => {
+    const { mes, hora, dia, anio } = fechaNacimiento();
     let edadAnio = fechaActual.anio - anio;
     let edadMes = fechaActual.mes - mes;
     let edadDia = fechaActual.dia - dia;
-    let edadHora = fechaActual.hora - hora;
-    if(edadAnio<fechaActual.anio){
+    /* 1 año tiene 8760 horas, un mes 730, y un dia 24. Son datos en promedio ya que hay años bisiestos */
+    let edadHora = (edadAnio * 8760)+ (edadMes * 730) + (edadDia * 24) + hora;
+
+    if (edadAnio < fechaActual.anio) {
         edadAnio--;
     }
-    if(edadMes<fechaActual.mes){
-        edadMes+=12;
+    if (edadMes < fechaActual.mes) {
+        edadMes += 12;
     }
-    if(edadDia<fechaActual.dia){
-        edadDia+=30;
-    }
-    if(edadHora<fechaActual.hora){
-        edadHora+=24;
+    if (edadDia < fechaActual.dia) {
+        edadDia += 30;
     }
 
-    document.body.innerHTML += `<h2>Tu edad es años: ${edadAnio}, meses: ${edadMes}, dias: ${edadDia}, semanas:${parseInt(edadDia/7)}, hora: ${edadHora}</h2>`;
+    document.body.innerHTML += `<h2>Tu edad es años: ${edadAnio}, meses: ${edadMes}, dias: ${edadDia}, semanas:${parseInt(edadDia / 7)}, horas: ${edadHora}</h2>`;
 }
